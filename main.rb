00bdddedd523ec44f1caf364dcc7a701447f6228
Rocco Basso <Rocco Basso>
2017-04-16 23:33:33 -0400

    styling updates to projects, about and contact pages; carousel animation

--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'pry'
+require 'mailgun'
 
 get '/' do
   erb :projects
@@ -10,4 +12,18 @@ end
 
 get '/contact' do
   erb :contact
+end
+
+post '/contact' do
+  binding.pry
+  mg_client = Mailgun::Client.new 'key-71aa3089c30f441a714c68fd8fef910d'
+
+  message_params = { from: 'robertbasso2@gmail.com',
+                    to:   'robertbasso2@gmail.com',
+                    subject: 'The Ruby SDK is awesome!',
+                    text:    'It is really easy to send a message!'
+                    }
+  binding.pry
+  mg_client.send_message 'gmail.com', message_params
+  binding.pry
 end
--- public/javascript/portfolio.js
+++ public/javascript/portfolio.js
@@ -1,16 +1,12 @@
 function showMenu() {
-  $('#dropdown-menu').css('margin-bottom', '22%');
-
   $('#small-screen-menu').slideDown(function() {
-    $('#close-menu-image').show();
-    $('#menu-image').hide();
+    $('#close-menu-image').show()
+    $('#menu-image').hide()
   });
 }
 
 function hideMenu() {
   $('#small-screen-menu').slideUp(function() {
-    $('#dropdown-menu').css('margin-bottom', '0');
-
     $('#close-menu-image').hide();
     $('#menu-image').show();
   });
@@ -18,7 +14,7 @@ function hideMenu() {
 
 if(document.querySelector(".carouselbox") != null) {
   carousel = (function(){
-    var box = document.querySelector(".carouselbox");
+    var box = document.querySelector('.carouselbox')
     var next = box.querySelector(".next");
     var prev = box.querySelector(".prev");
     var items = box.querySelectorAll(".content li");
